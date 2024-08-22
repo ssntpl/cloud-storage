bdde5e0179a663b4b8b7de283e14f69d2eda10c5
Abhishek Sharma <ilom31922@icicilombard.com>
2024-08-22 16:01:39 +0530

    optimize code and flow

--- src/Jobs/SyncFileJob.php
+++ src/Jobs/SyncFileJob.php
@@ -4,23 +4,32 @@ namespace Ssntpl\CloudStorage\Jobs;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\Storage;
+use Ssntpl\CloudStorage\CloudStorageAdapter;
 
-class SyncFileJob implements ShouldQueue
+class SyncFileJob implements ShouldQueue, ShouldBeUnique
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    // Set the maximum number of tries
+    public $tries = 3;
     protected $path;
+
+    protected $fromDisk;
+
+    protected $toDisk;
 
     /**
      * Create a new job instance.
      */
-    public function __construct($path)
+    public function __construct($path, $fromDisk, $toDisk)
     {
         $this->path = $path;
+        $this->fromDisk = $fromDisk;
+        $this->toDisk = $toDisk;
     }
 
     /**
@@ -30,6 +39,21 @@ class SyncFileJob implements ShouldQueue
      */
     public function handle()
     {
-        Storage::disk('cloud')->sync($this->path);
+        CloudStorageAdapter::syncToDisk($this->path, $this->fromDisk, $this->toDisk);
+    }
+
+    /**
+     * Get the unique ID for the job.
+     */
+    public function uniqueId(): string
+    {
+        return $this->path.'_'.$this->toDisk.'_sync';
+    }
+
+    public function backoff()
+    {
+        $interval = 1; // Set the interval in seconds for all retries
+
+        return array_fill(0, $this->tries - 1, $interval);
     }
 }
